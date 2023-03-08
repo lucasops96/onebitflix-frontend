755fd2befbaf1996d45ce17e34cc6e396dfa241d
João Lucas <lucasbanana96@gmail.com>
2023-03-08 20:19:10 -0300

    criando o formulário de registro - parte 1

--- pages/register.tsx
+++ pages/register.tsx
@@ -1,6 +1,9 @@
+/* eslint-disable @next/next/no-sync-scripts */
 import styles from "../styles/registerLogin.module.scss";
 import Head from "next/head";
 import HeaderGeneric from "@/src/components/common/headerGeneric";
+import { Container, Button, Form, FormGroup, Label, Input} from "reactstrap";
+import Footer from "../src/components/common/footer";
 
 const Register = function (){
     return(
@@ -8,9 +11,118 @@ const Register = function (){
         <Head>
             <title>Onebitflix - Registro</title>
             <link rel="shortcut icon" href="/favicon.svg" type="image/x-icon" />
+            <script src="https://jsuites.net/v4/jsuites.js"></script>
         </Head>
-        <main>
+        <main className={styles.main}>
             <HeaderGeneric logoUrl="/" btnUrl="/login" btnContent="Quero fazer login"/>
+            <Container className="py-5">
+                <p className={styles.formTitle}>Bem-vindo(a) ao Onebitflix!</p>
+                <Form className={styles.form}>
+                    <p className="text-center">
+                        <strong>Faça a sua conta!</strong>
+                    </p>
+                    <FormGroup>
+                        <Label for="firstName" className={styles.label} >
+                            NOME
+                        </Label>
+                        <Input 
+                            id="firstName" 
+                            name="firstName"
+                            type="text"
+                            placeholder="Qual seu nome?"
+                            required
+                            maxLength={24}
+                            className={styles.inputName}
+                        />
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="lastName" className={styles.label} >
+                            SOBRENOME
+                        </Label>
+                        <Input 
+                            id="lastName" 
+                            name="lastName"
+                            type="text"
+                            placeholder="Qual seu sobrenome?"
+                            required
+                            maxLength={24}
+                            className={styles.inputName}
+                        />
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="phone" className={styles.label} >
+                            WHASTAPP / TELEGRAM
+                        </Label>
+                        <Input 
+                            id="phone" 
+                            name="phone"
+                            type="tel"
+                            placeholder="(xx) 9xxxx-xxxx"
+                            data-mask="[-]+55 (00) 00000-0000"
+                            required
+                            className={styles.input}
+                        />
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="email" className={styles.label} >
+                            E-MAIL
+                        </Label>
+                        <Input 
+                            id="email" 
+                            name="email"
+                            type="email"
+                            placeholder="Digite o seu e-mail"
+                            required
+                            className={styles.input}
+                        />
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="birth" className={styles.label} >
+                            DATA DE NASCIMENTO
+                        </Label>
+                        <Input 
+                            id="birth" 
+                            name="birth"
+                            type="date"
+                            min="1930-01-01"
+                            max="2023-12-31"
+                            required
+                            className={styles.input}
+                        />
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="password" className={styles.label} >
+                            SENHA
+                        </Label>
+                        <Input 
+                            id="password" 
+                            name="password"
+                            type="password"
+                            placeholder="Digite a sua senha (Min: 6 | Max: 20)"
+                            required
+                            minLength={6}
+                            maxLength={20}
+                            className={styles.input}
+                        />
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="password" className={styles.label} >
+                            CONFIRME SUA SENHA
+                        </Label>
+                        <Input 
+                            id="password" 
+                            name="password"
+                            type="password"
+                            placeholder="Confirme a sua senha"
+                            required
+                            minLength={6}
+                            maxLength={20}
+                            className={styles.input}
+                        />
+                    </FormGroup>
+                </Form>
+            </Container>
+            <Footer/>
         </main>
         </>
     );
