c4f55307ea082b0d84749f6a28ca4989aeabe8d6
João Lucas <lucasbanana96@gmail.com>
2023-03-12 19:19:29 -0300

    Criando a estrutura da página homeAuth e seu head - Parte 2

--- src/components/common/headerAuth/index.tsx
+++ src/components/common/headerAuth/index.tsx
@@ -1,10 +1,30 @@
 /* eslint-disable @next/next/no-img-element */
 import Link from "next/link";
 import { Container, Form, Input } from "reactstrap";
+import { useState } from 'react'
 import styles from "./styles.module.scss";
+import Modal from "react-modal";
+import { useRouter } from "next/router";
 
+Modal.setAppElement("#__next");
 
 const HeaderAuth = function(){
+    const router = useRouter();
+    const [modalOpen,setModalOpen] = useState(false);
+
+    const handleOpenModal = ()=>{
+        setModalOpen(true);
+    };
+
+    const handleCloseModal = ()=>{
+        setModalOpen(false);
+    };
+
+    const handleLogout = ()=>{
+        sessionStorage.clear();
+        router.push("/");
+    };
+
     return(
         <>
             <Container className={styles.nav}>
@@ -29,8 +49,23 @@ const HeaderAuth = function(){
                         alt="lupaHeader" 
                         className={styles.searchImg}
                     />
-                     <p className={styles.userProfile} >AB</p>
+                     <p 
+                        className={styles.userProfile} 
+                        onClick={handleOpenModal} 
+                    >AB</p>
                 </div>
+                <Modal 
+                    isOpen={modalOpen}
+                    onRequestClose={handleCloseModal}
+                    shouldCloseOnEsc={true}
+                    className={styles.modal}
+                    overlayClassName={styles.overlayModal}
+                >
+                    <Link href="/profile" className="text-decoration-none">
+                        <p className={styles.modalLink} >Meus Dados</p>
+                    </Link>
+                    <p className={styles.modalLink} onClick={handleLogout} >Sair</p>
+                </Modal>
             </Container>
         </>
     );
