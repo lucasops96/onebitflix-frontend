8ef3e1b4f3e2eaa3264e32dc066b7326005ccb63
João Lucas <lucasbanana96@gmail.com>
2023-03-14 14:22:13 -0300

    Criação da seção de cursos em destaque - Parte 2

--- src/components/homeAuth/featuredSection/index.tsx
+++ src/components/homeAuth/featuredSection/index.tsx
@@ -18,13 +18,20 @@ const FeaturedSection = function (){
         <>
             {
                 data.data?.map((course:CourseType)=>(
-                    <div  key={course.id}>
+                    <div 
+                        style={{
+                            backgroundImage:`linear-gradient(to bottom,#6666661a,#151515),url(${process.env.NEXT_PUBLIC_BASEURL}/${course.thumbnailUrl})`,
+                            backgroundSize:"cover",
+                            backgroundPosition:"center",
+                            height:"480px"
+                        }}  
+                        key={course.id}>
                         <HeaderAuth/>
-                        <Container>
+                        <Container className="pt-4">
                             <p className={styles.title} >{course.name}</p>
                             <p className={styles.description} >{course.synopsis}</p>
-                            <Link href={`/courses/${course.id}`}>
-                                <Button outline color="light">
+                            <Link href={`/courses/${course.id}`} className="text-decoration-none">
+                                <Button outline color="light" className={styles.button}>
                                     ACESSE AGORA!
                                     <img 
                                         src="/buttonPlay.svg" 
