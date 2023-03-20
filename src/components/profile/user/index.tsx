60f5def26754c5aeab4130d87d02cb4070fe11a0
João Lucas <lucasbanana96@gmail.com>
2023-03-20 15:33:03 -0300

    Conexão com o backend usuário , user update- Parte 1

--- src/components/profile/user/index.tsx
+++ src/components/profile/user/index.tsx
@@ -1,14 +1,63 @@
 /* eslint-disable @next/next/no-img-element */
+import profileService from "../../../services/profileService";
+import { FormEvent, useEffect, useState } from "react";
 import { Button, Form, FormGroup, Input, Label } from "reactstrap";
 import styles from "../../../../styles/profile.module.scss";
+import ToastComponent from "../../common/toast";
 
 const UserForm = function(){
+    const [color,setColor] = useState("");
+    const [toastIsOpen,setToastIsOpen] = useState(false);
+    const [errorMessage,setErrorMessage] = useState("");
+    const [firstName,setFirstName] = useState("");
+    const [lastName,setLastName] = useState("");
+    const [phone,setPhone] = useState("");
+    const [email,setEmail] = useState("");
+    const [created_at,setCreated_at] = useState("");
+
+    useEffect(()=>{
+        profileService.fetchCurrent().then((user)=>{
+            setFirstName(user.firstName);
+            setLastName(user.lastName);
+            setPhone(user.phone);
+            setEmail(user.email);
+            setCreated_at(user.created_at);
+        });
+    },[]);
+
+    const handleUserUpdate = async function(event:FormEvent<HTMLFormElement>){
+        event.preventDefault();
+
+        const res = await profileService.userUpdate({
+            firstName,
+            lastName,
+            phone,
+            email,
+            created_at,
+        });
+
+        if(res === 200){
+            setToastIsOpen(true);
+            setErrorMessage("Informações alteradas com sucesso!");
+            setColor("bg-success");
+            setTimeout(()=> setToastIsOpen(false), 1000 * 3)
+        }else{
+            setToastIsOpen(true);
+            setErrorMessage("Você não pode mudar para esse e-amil!");
+            setColor("bg-danger");
+            setTimeout(()=> setToastIsOpen(false), 1000 * 3)
+        }
+    };
+
     return(
         <>
-            <Form className={styles.form}>
+            <Form onSubmit={handleUserUpdate} className={styles.form}>
                 <div className={styles.formName}>
-                    <p className={styles.nameAbbreviation}>NT</p>
-                    <p className={styles.userName}>NAME TEST</p>
+                    <p className={styles.nameAbbreviation}>
+                        {firstName.slice(0,1)}
+                        {lastName.slice(0,1)}
+                    </p>
+                    <p className={styles.userName}>{`${firstName} ${lastName}`}</p>
                 </div>
                 <div className={styles.memberTime}>
                     <img 
@@ -33,7 +82,10 @@ const UserForm = function(){
                             required
                             maxLength={20}
                             className={styles.inputFlex}
-                            value={"Name"}
+                            value={firstName}
+                            onChange={(event)=>{
+                                setFirstName(event.target.value);
+                            }}
                         />
                     </FormGroup>
                     <FormGroup>
@@ -48,7 +100,10 @@ const UserForm = function(){
                             required
                             maxLength={20}
                             className={styles.inputFlex}
-                            value={"Test"}
+                            value={lastName}
+                            onChange={(event)=>{
+                                setLastName(event.target.value);
+                            }}
                         />
                     </FormGroup>
                 </div>
@@ -63,7 +118,10 @@ const UserForm = function(){
                             placeholder="(xx) 9xxxx-xxxx"
                             required
                             className={styles.input}
-                            value={"+55 (21) 99999-9999"}
+                            value={phone}
+                            onChange={(event)=>{
+                                setPhone(event.target.value);
+                            }}
                         />
                     </FormGroup>
                     <FormGroup>
@@ -76,7 +134,10 @@ const UserForm = function(){
                             placeholder="Coloque seu e-mail"
                             required
                             className={styles.input}
-                            value={"testeemail@gmail.com"}
+                            value={email}
+                            onChange={(event)=>{
+                                setEmail(event.target.value);
+                            }}
                         />
                     </FormGroup>
 
@@ -85,6 +146,11 @@ const UserForm = function(){
                     </Button>
                 </div>
             </Form>
+            <ToastComponent
+                color={color}
+                isOpen={toastIsOpen}
+                message={errorMessage}
+            />
         </>
     );
 };
