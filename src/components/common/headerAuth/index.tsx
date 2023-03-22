e2e057c6979c53c7ea371c40f7e13d986627512c
João Lucas <lucasbanana96@gmail.com>
2023-03-22 14:43:13 -0300

    Backend do Search

--- src/components/common/headerAuth/index.tsx
+++ src/components/common/headerAuth/index.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable @next/next/no-img-element */
 import Link from "next/link";
 import { Container, Form, Input } from "reactstrap";
-import { useEffect, useState } from 'react'
+import { FormEvent, useEffect, useState } from 'react'
 import styles from "./styles.module.scss";
 import Modal from "react-modal";
 import { useRouter } from "next/router";
@@ -13,6 +13,19 @@ const HeaderAuth = function(){
     const router = useRouter();
     const [modalOpen,setModalOpen] = useState(false);
     const [initials,setInitials] = useState("");
+    const [searchName,setSearchName] = useState("");
+
+    const handleSearch = async (event:FormEvent<HTMLFormElement>)=>{
+        event.preventDefault();
+
+        router.push(`search?name=${searchName}`);
+        setSearchName("");
+    };
+
+    const handleSearchClick = ()=>{
+        router.push(`search?name=${searchName}`);
+        setSearchName("");
+    };
 
     useEffect(()=>{
         profileService.fetchCurrent().then((user)=>{
@@ -46,18 +59,23 @@ const HeaderAuth = function(){
                     />
                 </Link>
                 <div className="d-flex align-items-center">
-                    <Form>
+                    <Form onSubmit={handleSearch}>
                         <Input 
                             name="search" 
                             type="search"
                             placeholder="Pesquisar"
                             className={styles.input}
+                            value={searchName}
+                            onChange={(event)=>{
+                                setSearchName(event.currentTarget.value.toLowerCase());
+                            }}
                         />
                     </Form>
                     <img 
                         src="/homeAuth/iconSearch.svg" 
                         alt="lupaHeader" 
                         className={styles.searchImg}
+                        onClick={handleSearchClick}
                     />
                      <p 
                         className={styles.userProfile} 
